c66de60d0ac1c9c40b96bedf7ce9bbf78f88530b
Ellisvelandia <eyis619l@gmail.com>
2023-02-25 19:52:25 -0500

    testting arrays

--- src/base-pruebas/05-funciones.js
+++ src/base-pruebas/05-funciones.js
@@ -1,14 +1,15 @@
 const getUser = () => ({
-  uid: "ABC123",
-  username: "El_Papi1502",
-});
-
-const getUsuarioActivo = (nombre) => ({
-  uid: "ABC567",
-  username: nombre,
-});
-
-module.exports = {
-  getUser,
-  getUsuarioActivo,
-};
+    uid: "ABC123",
+    username: "El_Papi1502",
+  });
+  
+  const getUsuarioActivo = (nombre) => ({
+    uid: "ABC567",
+    username: nombre,
+  });
+  
+  module.exports = {
+    getUser,
+    getUsuarioActivo,
+  };
+  
